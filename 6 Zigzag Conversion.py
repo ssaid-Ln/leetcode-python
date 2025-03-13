931ecf89dbbde9f506841ebff34dcbd0a25e65f0
Said Ln <141834518+ssaid-Ln@users.noreply.github.com>
2025-03-13 23:26:57 +0100

    Solution of question 6

---
+++ 6 Zigzag Conversion.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+        
+        rows = ["" for _ in range(numRows)]
+        index, step = 0, 1
+        
+        for char in s:
+            rows[index] += char
+            
+            if index == 0:
+                step = 1  # Move down
+            elif index == numRows - 1:
+                step = -1  # Move up
+            
+            index += step
+        
+        return "".join(rows)
